f35e7d439114702641c40f50eab901557b7d14e0
malibu911 <arkadev.sx@gmail.com>
2020-08-28 15:26:01 +0300

    scss

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./js/common";
+import "./css/main.css"
+import "./scss/main.scss"
+// папулярна
+// import "vue";
+
+// если надо чота сделать
+// import Vue from 'vue'
+// глобаЛЬНО не делай так ..
+window.Vue = require('vue') 
+
